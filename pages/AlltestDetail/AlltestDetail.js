90523d50d64ecf5ee59dc2f3f025d2883ddbe49e
xxj <1013002924@qq.com>
2021-05-13 17:53:15 +0800

    answer logic

--- pages/AlltestDetail/AlltestDetail.js
+++ pages/AlltestDetail/AlltestDetail.js
@@ -34,8 +34,259 @@ Page({
     fullScreen: '',
     coverView:true,
     examuserId:'',
-    questionArray:''
+    questionArray:'',
+    canshowAllquestions:false,
+    allquestionsData:'',
+    questionId: '',
+    optionId: '',
+    focus: false,
+    flag: false,
   },
+  jumpTosomeoneQuestion(options){
+    var that = this;
+   console.log("options",options)
+   wx.request({
+    url: app.globalData.URi + '/applets/exam/pagination.jspx', //自己的服务接口地址
+    method: 'post',
+    data: {
+      examuserId: that.data.examuserId,
+      pageNo:options.currentTarget.dataset.index
+    },
+    header: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    success: function (res) {
+      console.log("res=>", res.data)
+      that.setData({
+        questionArray:res.data.array,
+        totalpage:res.data.totalPage,
+        pageno:options.currentTarget.dataset.index
+      })
+    },
+    fail: function () {
+      console.log('系统错误！')
+    }
+  })
+ that.CloseAllquestionspanel();
+
+  },
+
+      /**
+   * 
+   * 答题卡
+   */
+  getAllquestions(){
+    var that =this;
+    that.setData({
+      canshowAllquestions:true
+    })
+    wx.request({
+      url: app.globalData.URi + '/applets/exam/all.jspx', //自己的服务接口地址
+      method: 'post',
+      data: {
+        examuserId: that.data.examuserId,
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        console.log("res=>", res)
+        that.setData(
+          {
+            allquestionsData:res.data.array
+          }
+        )
+        for(var i = 0;i<res.data.array.length;i++){
+                
+        }
+      },
+      fail: function () {
+        console.log('系统错误！')
+      }
+    })
+  },
+        /**
+   * 
+   * 关闭答题卡
+   */
+  CloseAllquestionspanel(){
+    var that =this;
+    that.setData({
+      canshowAllquestions:false
+    })
+  },
+  /**
+   * 
+   * 单项选择
+   */
+  radiochange: function (e) {
+ 
+    var item = e.detail.value //  
+
+    console.log('radio发生change事件，携带的value值为：', e.detail.value);
+   
+    //循环选中的数组，取出对应的数据进行数组拼接 
+    // for (var i = 0; i < item.length; i++) {
+    var row = item.split(",")
+
+    console.log("row=>",row)
+    //将数组进行分割 
+    var questionId = row[0] //数组下表的第一个
+    var optionId = row[1].concat(',')
+    this.setData({
+      optionId: optionId,
+      questionId:questionId
+    })
+
+    wx.request({
+      url: app.globalData.URi + '/applets/exam/option_save.jspx', //自己的服务接口地址
+      method: 'post',
+      data: {
+      userId:wx.getStorageSync('userId'),
+      options: this.data.optionId,
+      themeId:this.data.questionId
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        console.log(res);
+        console.log(res.data);
+     
+      }
+    })
+  },
+  /**
+   * 
+   * 多项选择
+   */
+  check(e) {
+    let questionId = e.currentTarget.dataset.id;
+    console.log("当前checkBox事件：", e.currentTarget.dataset.id);
+    this.setData({
+      questionId: questionId
+    })
+    if(this.data.flag==true){
+      this.setData({
+        questionId: questionId,
+        optionId: '',
+      })
+      wx.request({
+        url: app.globalData.URi + '/applets/exam/option_save.jspx', //自己的服务接口地址
+        method: 'post',
+        data: {
+          userId:wx.getStorageSync('userId'),
+          options: this.data.optionId,
+          themeId:this.data.questionId
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        success: function (res) {
+          console.log(res.data);
+          
+        }
+
+      })
+     }
+  },
+  checkboxChange(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail)
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+
+    var item = e.detail.value //
+
+    var questionId = [];
+
+    var optionId = []; //  选中的NAME
+
+    //循环选中的数组，取出对应的数据进行数组拼接 
+    for (var i = 0; i < item.length; i++) {
+      var row = item[i].split(",")
+      //将数组进行分割 
+      questionId = questionId.concat(row[0]) //数组下表的第一个为
+      optionId = optionId.concat(row[1])
+      //数组下表的第二个为name 
+    }
+    console.log("多选事件", questionId);
+    console.log("多选事件", optionId);
+    if (optionId.length > 0) {
+      var questionId = row[0] //数组下表的第一个为
+      var optionId = optionId
+      this.setData({
+        questionId: questionId,
+        optionId: optionId,
+        flag :false
+      })
+      wx.request({
+        url: app.globalData.URi + '/applets/exam/option_save.jspx', //自己的服务接口地址
+        method: 'post',
+        data: {
+          userId:wx.getStorageSync('userId'),
+          options: this.data.optionId,
+          themeId:this.data.questionId
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        success: function (res) {
+          console.log(res.data);
+         
+        }
+      })
+    } else {
+      this.setData({
+        flag :true
+      })
+      console.log("数组为空，多选数组为空");
+    }
+  },
+      /**
+   * 
+   * textarea绑定事件
+   */
+  handleJumpPage(e){
+    var that=this;
+    let questionId = e.currentTarget.dataset.id;
+    console.log("当前handleJumpPage事件：", e.currentTarget.dataset.id);
+      that.setData({
+        questionId:e.currentTarget.dataset.id
+      })
+  },
+    /**
+   * 
+   * textarea
+   */
+  bindTextAreaBlur: function (e) {
+    var that = this;
+    console.log("okok");
+    console.log(e.detail.value);
+    this.setData({
+      remark:e.detail.value
+    })
+    console.log("remark类型：",typeof(that.data.remark))
+    wx.request({
+      url: app.globalData.URL + '/app/member-option-save.jspx', //自己的服务接口地址
+      method: 'post',
+      data: {
+        accountId: that.data.accountId,
+        optionIds: that.data.optionIds,
+        questionId:that.data.questionId,
+        remark:that.data.remark
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        console.log(res.data);
+       
+      }
+    })
+  },
+  onChangeinput(e){
+  console.log("e=>",e)
+  },
+  onChangeinputclick(){},
     /**
    * 
    * 上一题
